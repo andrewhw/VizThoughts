0385dd8355571248c9792cfbf4f3125d0651d90f
Andrew Wright <andrew.hamilton-wright@uoguelph.ca>
2023-06-26 11:22:57 -0400

    Update anscombe_hue+style.py
    Fixed wandering comment.

--- examples/anscombe_hue+style.py
+++ examples/anscombe_hue+style.py
@@ -20,13 +20,13 @@ df = sns.load_dataset("anscombe")
 df = df.rename(columns={"x":"x (unitless)", "y":"y (unitless)"})
 
 
-## use hue (colour) AND point style to differentiate the data sets
-
 # get the figure when the canvas is blank -- once drawn, we can save it
 fig = plt.figure()
 
 # Draw on the canvas using a seaborn plotting tool.
 # This creates a scatter plot for a single data set
+#
+# use hue (colour) AND point style to differentiate the data sets
 sns.scatterplot(x = "x (unitless)", y = "y (unitless)",
 		style="dataset", hue="dataset", alpha=0.65,
 		data=df)
